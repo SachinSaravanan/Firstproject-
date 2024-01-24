7e3e9a6849ab460d7f63fc4d2fcaa56d5d62c54e
unknown <ssachinsachin113@gmail.com>
2024-01-24 09:57:16 +0530

    pushing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillComponent } from './skill/skill.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PortfolioComponent, SkillComponent ,ContactComponent,FooterComponent  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
